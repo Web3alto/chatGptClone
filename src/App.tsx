160a07f69a2da8086e2de7d02827b533fb724ed5
Web3Alto <web3alto@gmail.com>
2023-08-30 20:34:13 +0200

    commit font + history

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,23 @@ function App() {
 							</svg>
 						</div>
 					</div>
-					<div className="history"></div>
+					<div className="history">
+						<p>Today</p>
+						<div className="content">
+							<svg
+								stroke="currentColor"
+								fill="none"
+								stroke-width="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+							</svg>
+							<h4>Latest Research</h4>
+						</div>
+					</div>
 					<div className="profile">
 						<div className="pfp"></div>
 						<div className="id">alto</div>
