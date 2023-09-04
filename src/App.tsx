2d575c398deaa2bf3ee347150e4ffef3cb6a5571
Web3Alto <web3alto@gmail.com>
2023-09-04 15:28:26 +0200

    commit toggle sidebar + tooltip help

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+	const [tooltipActive, setTooltipActive] = useState(false);
+
+	const toggleTooltip = () => {
+		setTooltipActive(!tooltipActive);
+	};
+
+	const [navActive, setNavActive] = useState(false);
+
+	const toggleNav = () => {
+		setNavActive(!navActive);
+	};
+
+	const closeNav = () => {
+		setNavActive(false);
+	};
+
 	return (
 		<>
 			<div className="container">
-				<nav>
+				<nav className={navActive ? "active" : ""}>
 					<div className="option">
 						<div className="new-chat">
 							<svg
@@ -21,7 +38,7 @@ function App() {
 							</svg>
 							<p>New Chat</p>
 						</div>
-						<div className="toggle-nav">
+						<div className="toggle-nav" onClick={toggleNav}>
 							<svg
 								stroke="currentColor"
 								fill="none"
@@ -61,6 +78,76 @@ function App() {
 							</svg>
 							<h4>Latest Research</h4>
 						</div>
+						<div className="content">
+							<svg
+								stroke="currentColor"
+								fill="none"
+								stroke-width="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+							</svg>
+							<h4>Latest Research</h4>
+						</div>
+						<div className="content">
+							<svg
+								stroke="currentColor"
+								fill="none"
+								stroke-width="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+							</svg>
+							<h4>Latest Research</h4>
+						</div>
+						<div className="content">
+							<svg
+								stroke="currentColor"
+								fill="none"
+								stroke-width="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+							</svg>
+							<h4>Latest Research</h4>
+						</div>
+						<div className="content">
+							<svg
+								stroke="currentColor"
+								fill="none"
+								stroke-width="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+							</svg>
+							<h4>Latest Research</h4>
+						</div>
+						<div className="content">
+							<svg
+								stroke="currentColor"
+								fill="none"
+								stroke-width="2"
+								viewBox="0 0 24 24"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+							</svg>
+							<h4>Latest Research</h4>
+						</div>
 					</div>
 					<div className="profile">
 						<div className="pfp"></div>
@@ -82,7 +169,30 @@ function App() {
 						</div>
 					</div>
 				</nav>
-				<div className="main">
+				<div className={`main ${navActive ? "active" : ""}`}>
+					<div className="show-nav" onClick={closeNav}>
+						<svg
+							stroke="currentColor"
+							fill="none"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							height="1em"
+							width="1em"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<rect
+								x="3"
+								y="3"
+								width="18"
+								height="18"
+								rx="2"
+								ry="2"
+							></rect>
+							<line x1="9" y1="3" x2="9" y2="21"></line>
+						</svg>
+					</div>
 					<div className="top">
 						<div className="title">
 							<h2>ChatGPT</h2>
@@ -110,11 +220,76 @@ function App() {
 							</svg>
 						</div>
 					</div>
-					<div className="help">
-						<img
-							src="/assets/point-dinterrogation.png"
-							alt="point-dinterrogation"
-						/>
+					<div
+						className={`tooltip ${tooltipActive ? "active" : ""}`}
+						onClick={toggleTooltip}
+					>
+						<div className="help">
+							<img
+								src="/assets/point-dinterrogation.png"
+								alt="point-dinterrogation"
+							/>
+							<div className="tooltip-text">
+								<div className="faq">
+									<svg
+										stroke="currentColor"
+										fill="none"
+										stroke-width="2"
+										viewBox="0 0 24 24"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+										<polyline points="15 3 21 3 21 9"></polyline>
+										<line
+											x1="10"
+											y1="14"
+											x2="21"
+											y2="3"
+										></line>
+									</svg>
+									<h3>Help & FAQ</h3>
+								</div>
+								<div className="shortcut">
+									<svg
+										stroke="currentColor"
+										fill="none"
+										stroke-width="2"
+										viewBox="0 0 24 24"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<rect
+											x="3"
+											y="3"
+											width="7"
+											height="7"
+										></rect>
+										<rect
+											x="14"
+											y="3"
+											width="7"
+											height="7"
+										></rect>
+										<rect
+											x="14"
+											y="14"
+											width="7"
+											height="7"
+										></rect>
+										<rect
+											x="3"
+											y="14"
+											width="7"
+											height="7"
+										></rect>
+									</svg>
+									<h3>Keyboard shortcuts</h3>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
