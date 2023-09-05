d81730926db2d48652469ff1babe9d8862260f56
Web3Alto <web3alto@gmail.com>
2023-09-05 18:40:38 +0200

    commit history logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
 	const [tooltipActive, setTooltipActive] = useState(false);
+	const [navActive, setNavActive] = useState(false);
+	const [activeContentIndex, setActiveContentIndex] = useState<number | null>(
+		null
+	);
+
+	const [contentItems, setContentItems] = useState([
+		{
+			title: "Latest Research",
+			active: true, // Set the first item as active by default
+		},
+	]);
+
+	useEffect(() => {
+		// Set the initial active content index when the component mounts
+		setActiveContentIndex(0);
+	}, []);
 
 	const toggleTooltip = () => {
 		setTooltipActive(!tooltipActive);
 	};
-
-	const [navActive, setNavActive] = useState(false);
 
 	const toggleNav = () => {
 		setNavActive(!navActive);
@@ -18,12 +32,34 @@ function App() {
 		setNavActive(false);
 	};
 
+	const handleContentClick = (index: number) => {
+		setActiveContentIndex(index);
+		setContentItems((prevItems) =>
+			prevItems.map((item, i) => ({
+				...item,
+				active: i === index,
+			}))
+		);
+	};
+
+	const addContentItem = () => {
+		setContentItems((prevItems) => [
+			...prevItems,
+			{ title: "New Content", active: false },
+		]);
+	};
+
+	const removeContentItem = (index: number) => {
+		setContentItems((prevItems) => prevItems.filter((_, i) => i !== index));
+		setActiveContentIndex(null);
+	};
+
 	return (
 		<>
 			<div className="container">
 				<nav className={navActive ? "active" : ""}>
 					<div className="option">
-						<div className="new-chat">
+						<div className="new-chat" onClick={addContentItem}>
 							<svg
 								stroke="currentColor"
 								fill="none"
@@ -64,90 +100,75 @@ function App() {
 					</div>
 					<div className="history">
 						<p>Today</p>
-						<div className="content">
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
+						{contentItems.map((item, index) => (
+							<div
+								key={index}
+								className={`content ${
+									item.active ? "selected" : ""
+								}`}
+								onClick={() => handleContentClick(index)}
 							>
-								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-							</svg>
-							<h4>Latest Research</h4>
-						</div>
-						<div className="content">
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-							</svg>
-							<h4>Latest Research</h4>
-						</div>
-						<div className="content">
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-							</svg>
-							<h4>Latest Research</h4>
-						</div>
-						<div className="content">
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-							</svg>
-							<h4>Latest Research</h4>
-						</div>
-						<div className="content">
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-							</svg>
-							<h4>Latest Research</h4>
-						</div>
-						<div className="content">
-							<svg
-								stroke="currentColor"
-								fill="none"
-								stroke-width="2"
-								viewBox="0 0 24 24"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-							</svg>
-							<h4>Latest Research</h4>
-						</div>
+								<svg
+									stroke="currentColor"
+									fill="none"
+									strokeWidth="2"
+									viewBox="0 0 24 24"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+								</svg>
+								<h4>{item.title}</h4>
+								{item.active && (
+									<div className="icon-active">
+										<svg
+											stroke="currentColor"
+											fill="none"
+											stroke-width="2"
+											viewBox="0 0 24 24"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											class="h-4 w-4"
+											height="1em"
+											width="1em"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path d="M12 20h9"></path>
+											<path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
+										</svg>
+										<svg
+											className="content-icon trashbin"
+											stroke="currentColor"
+											fill="none"
+											strokeWidth="2"
+											viewBox="0 0 24 24"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											xmlns="http://www.w3.org/2000/svg"
+											onClick={() =>
+												removeContentItem(index)
+											}
+										>
+											<polyline points="3 6 5 6 21 6"></polyline>
+											<path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+											<line
+												x1="10"
+												y1="11"
+												x2="10"
+												y2="17"
+											></line>
+											<line
+												x1="14"
+												y1="11"
+												x2="14"
+												y2="17"
+											></line>
+										</svg>
+									</div>
+								)}
+							</div>
+						))}
 					</div>
 					<div className="profile">
 						<div className="pfp"></div>
